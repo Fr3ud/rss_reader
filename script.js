805e031d35d050d76a5fc8a06be4a4db6ff62beb
Fr3ud <oo.pavelko@gmail.com>
2018-05-06 22:56:02 +0300

    rss reader

---
+++ script.js
@@ -0,0 +1,29 @@
+(function (window, document) {
+    function Articles() {
+        this.articles = {};
+    }
+
+    Articles.prototype.init = function () {
+
+    };
+
+    Articles.prototype.getData = function () {
+
+    };
+
+    Articles.prototype.renderAll = function () {
+
+    };
+
+    Articles.prototype.generateAll = function () {
+
+    };
+
+    Articles.prototype.generateArticle = function () {
+
+    };
+
+    const articles = new Articles();
+
+    articles.init();
+})(window, document);
